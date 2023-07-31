b0f80ff2c18ff02662e561a3ecc46830d610b571
EmersonSinezio <emerson.sineziio@gmail.com>
2023-07-31 10:14:42 -0300

    fix_links

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,8 +2,12 @@
     <div class="header_container">
         <h1 class="header_logo">Portfólio</h1>
         <ul class="header_menu">
-            <li>Github</li>
-            <li>Linkedin</li>
+            <a href="https://www.linkedin.com/in/emerson-sineziio" target="_blank">
+                <li>Linkedin</li>
+            </a>
+            <a href="https://github.com/EmersonSinezio" target="_blank">
+                <li>Github</li>
+            </a>
         </ul>
     </div>
 </template>
--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -11,7 +11,9 @@
             <p>Bem-vindo ao universo épico dos games! Descubra as aventuras de God of War, enfrente os horrores de A
               Plague Tale: Requiem e mergulhe na ação de GTA 5. Seja você um jogador casual ou hardcore, nosso site é o
               destino perfeito para os apaixonados por jogos.</p>
-            <button>Visitar</button>
+              <a href="https://games-store-eta.vercel.app/#/" target="blank">
+                <button>Visitar</button>
+              </a>
           </div>
         </div>
         <div class="project">
@@ -23,29 +25,39 @@
             <p>Descubra o mundo dos sapatos dos sonhos! Encontre o par perfeito para cada ocasião. De confortáveis a
               estilosos, nosso e-commerce tem tudo que você precisa para arrasar no look. Não perca mais tempo, venha
               explorar a coleção exclusiva de sapatos em nosso site agora</p>
-            <button>Visitar</button>
+              <a href="https://sneakers-opal-alpha.vercel.app" target="blank">
+                <button>Visitar</button>
+              </a>
           </div>
         </div>
       </div>
       <div :class="`project_container ${show2}`">
         <div class="project">
           <div class="project_image">
-            <img src="../assets/Foody_home.png" alt="">
+            <img src="../assets/Foody_home.png" alt="Foody_image">
           </div>
           <div>
             <h3>Projeto Foody</h3>
-            <p>Explore sabores irresistíveis! Delicie-se com nossa seleção de pratos gourmet, receitas tradicionais e guloseimas únicas. Uma experiência gastronômica que vai encantar seus sentidos. Visite agora e embarque em uma jornada culinária inesquecível</p>
-            <button>Visitar</button>
+            <p>Explore sabores irresistíveis! Delicie-se com nossa seleção de pratos gourmet, receitas tradicionais e
+              guloseimas únicas. Uma experiência gastronômica que vai encantar seus sentidos. Visite agora e embarque em
+              uma jornada culinária inesquecível</p>
+              <a href="https://foody-pearl-alpha.vercel.app" target="blank">
+                <button>Visitar</button>
+              </a>
           </div>
         </div>
         <div class="project">
-          <div class="project_image">img</div>
+          <div class="project_image">
+            <img src="../assets/clock_homepage.png" alt="clock_project">
+          </div>
           <div>
-            <h3>Description</h3>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit dolores quos praesentium
-              similique
-              inventore ab?</p>
-            <button>Visitar</button>
+            <h3>Projeto Clock</h3>
+            <p>Bem-vindo ao Projeto Clock, um projeto encantador que vai além de um simples relógio. O Clock oferece um cronômetro preciso e um timer embutido,
+              tornando-o perfeito para medir o tempo em atividades desafiadoras ou momentos de lazer, mantendo seu foco e
+              produtividade em alta.</p>
+            <a href="https://clock-three-kappa.vercel.app" target="blank">
+              <button>Visitar</button>
+            </a>
           </div>
         </div>
       </div>
