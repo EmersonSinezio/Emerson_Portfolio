596b6f449f608e2e32aa46cd7c592677e6fca156
EmersonSinezio <emerson.sineziio@gmail.com>
2025-05-01 18:37:50 -0300

    🚀Feat: Adding form submission to the database

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import Header from "./components/Header";
 import Projects from "./pages/Projects";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import Admin from "./pages/Admin";
 export default function App() {
   return (
     <div>
@@ -15,7 +14,6 @@ export default function App() {
           <Route path="/projects" element={<Projects />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
-          <Route path="/admin" element={<Admin />} />
           <Route path="*" element={<h1>404</h1>} />
         </Routes>
       </Router>
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,27 +1,93 @@
 import { FaGithub, FaLinkedinIn, FaWhatsapp } from "react-icons/fa";
 import { IoIosMail } from "react-icons/io";
+import { toast, Zoom, ToastContainer } from "react-toastify";
+import api from "../api/api";
 
 export default function Contact() {
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const form = e.currentTarget;
+    const formData = new FormData(form);
+
+    const sendMessage = {
+      name: `${formData.get("firstName")}_${formData.get("lastName")}`,
+      email: formData.get("email"),
+      message: formData.get("message"),
+    };
+
+    const promise = api.post("/send", sendMessage);
+
+    toast.promise(promise, {
+      pending: {
+        render: "Enviando mensagem...",
+        position: "top-center",
+        transition: Zoom,
+        theme: "colored",
+      },
+      success: {
+        render: "Mensagem enviada com sucesso!",
+        position: "top-center",
+        autoClose: 3000,
+        transition: Zoom,
+        theme: "colored",
+        hideProgressBar: false,
+        closeOnClick: false,
+      },
+      error: {
+        render: ({ data }) =>
+          `Erro ao enviar: ${
+            (data as any).message || "Tente novamente mais tarde"
+          }`,
+        position: "top-center",
+        transition: Zoom,
+        theme: "colored",
+      },
+    });
+
+    try {
+      await promise;
+      form.reset();
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <div className="contact-container">
       <div className="contact-left">
+        <ToastContainer />
         <h1>Entre em contato</h1>
-        <form action="">
+        <form onSubmit={handleSubmit}>
           <div className="name-container">
             <div>
-              <label htmlFor="">Primeiro nome</label>
-              <input type="text" placeholder="Primeiro nome" />
+              <label>Primeiro nome</label>
+              <input
+                name="firstName"
+                type="text"
+                placeholder="Primeiro nome"
+                required
+              />
             </div>
             <div>
-              <label htmlFor="">Sobrenome</label>
-              <input type="text" placeholder="Sobrenome" />
+              <label>Sobrenome</label>
+              <input
+                name="lastName"
+                type="text"
+                placeholder="Sobrenome"
+                required
+              />
             </div>
           </div>
-          <label htmlFor="">Email</label>
-          <input type="email" placeholder="Seu_email@dominio.com" />
-          <label htmlFor="">Mensagem</label>
-          <textarea name="" id="" placeholder="Digite sua mensagem"></textarea>
-          <input type="submit" name="" id="" value={"Enviar"} />
+          <label>Email</label>
+          <input
+            name="email"
+            type="email"
+            placeholder="Seu_email@dominio.com"
+            required
+          />
+          <label>Mensagem</label>
+          <textarea name="message" placeholder="Digite sua mensagem" required />
+          <input type="submit" value="Enviar" />
         </form>
         <div className="contact-icons">
           <a href="">
